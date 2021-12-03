eb18b3b50298fef9e04fb9d63d4fcb4d012b12c3
Arjun Syah <engkukbae@gmail.com>
2021-12-03 17:46:02 +0700

    menambahkan file

--- datatelp.py
+++ datatelp.py
@@ -1,16 +1,6 @@
-# Labspy05
-# Latihan1
-## Membuat daftar kontak dengan menggunakan dicctionary pada python
-### Berikut Programnya
-![gambar1](ss/s0.PNG)
-- Dengan keterangan
-```python
 daftarkontak = {"Nama":"Nomor Telepon"}
 kontak = {"Arjun Syah":"0855915297998", "Siti Nuraisah":"085287997552"}
-```
-- diatas adalah untuk menampung data dari kamus
-### kondisi 1
-```python
+
 print("="*50)
 print("   Nama              |    Nomor Telepon   " )
 print("="*50)
@@ -20,12 +10,8 @@ print("-"*50)
 print()
 print()
 print("="*50)
-```
-- untuk mengakses atau menampilkan kontak yang telah ditampung dalam kamus
-### Berikut hasil dari programnya
-![gambar2](ss/s1.PNG)
-### kondisi 2
-```python
+
+# Menampilkan kontak Arjun Syah
 print("Menampilkan Kontak Arjun Syah")
 print("="*50)
 print(" # Arjun Syah        |   ", kontak["Arjun Syah"])
@@ -33,12 +19,8 @@ print("-"*50)
 print()
 print()
 print("="*50)
-```
-- Kode diatas adalah untuk menampilkan salah satu daftar kontak yang ada
-### Berikut hasil programnya
-![gambar3](ss/s2.PNG)
-### Kondisi 3
-```python
+
+# Menambahkan kontak dengna nama haikal 
 print("Menambahkan kontak dengan Nama Haikal")
 print("dengan Nomor telepon 085215376070")
 kontak["Haikal"]="085215376070"
@@ -48,12 +30,8 @@ print("-"*50)
 print()
 print()
 print("="*50)
-```
-- kode diatas untuk menambahkan konntak 
-### Berikut hasil programnya
-![gambar4](ss/s3.PNG)
-### kondisi 4
-```python
+
+# Mengubah Nomor Siti Nuraisah dengan Nomor baru
 print("Mengubah Nomor Siti Nuraisah dengan Nomor 08521267890")
 print("="*50)
 print(" # Siti Nuraisah     |    ", kontak["Siti Nuraisah"])
@@ -61,12 +39,18 @@ print("-"*50)
 print()
 print()
 print("="*50)
-```
-- Kode diatas untuk mengubah kontak
-### Berikut hasil programnya
-![gambar5](ss/s4.PNG)
-### Kondisi 5
-```python
+
+
+# Menampilkan semua Nama
+print("Menampilkan Semua Nama dalam Kontak")
+print("="*50)
+print(kontak.keys())
+print("-"*50)
+print()
+print()
+print("="*50)
+
+# Menampilkan semua Nomor
 print("Menampilkan semua Nomor dalam Konntak")
 print("="*50)
 print(kontak.values())
@@ -74,12 +58,8 @@ print("-"*50)
 print()
 print()
 print("="*50)
-```
-- kode diatas adalah untuk menampilkan semua nomor yang ada
-### Berikut hasil programnya
-![gambar6](ss/s5.PNG)
-### kondisi 6
-```python
+
+# Menampilkan semua daftar kontak
 print("Menampilkan Semua Daftar Kontak")
 print("="*50)
 print(kontak.items())
@@ -87,12 +67,9 @@ print("-"*50)
 print()
 print()
 print("="*50)
-```
-- Kode diatas adalah untuk menampilkan semua daftar dalam kontak
-### Berikut hasil programnnya
-![gambar7](ss/ss6.PNG)
-### Kondisi 7
-```python
+
+
+# Menghapus Salah satu Kontak
 print("Hapus Kontak Haikal")
 print("="*50)
 kontak.pop("Haikal") 
@@ -100,8 +77,6 @@ print(kontak.items())
 print("-"*50)
 print()
 print()
-```
-- kode di atas adalah untuk menghapus salah satu dari konntak yang ada
-### Berikut hasil programnya
-![gambar8](ss/s7.PNG)
-## latihan1 selesai
+
+
+
